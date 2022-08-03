70f4d6a8d513083106b63af65ecc991879d0abe4
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2022-08-03 17:48:36 -0500

    api

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Grupo Nach</title>
+        <link rel="stylesheet" href="/resources/css/app.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <style>
+      body {
+        font-family: 'Lato', serif;
+      }
+    </style>
+    <script src="https://kit.fontawesome.com/6c9783d4e5.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Grupo Nach</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Bolsa de trabajo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa-solid fa-user"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <section>
+            <!-- cards -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>                                
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-heart"></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-comment"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-eye"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-heart"></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-comment"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-eye"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-heart"></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-comment"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                    <div class="col-4">
+                                        <small class="text-muted"><i class="fa-solid fa-eye"></i></i></small>
+                                        <small class="text-muted">57</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <header class="center">
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+            </header>
+            <div class="row">
+                <div class="col-8">
+                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="w-100 h-100" alt="...">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae architecto, a aliquam itaque vitae, ab minima excepturi labore beatae veniam ipsa temporibus commodi cum consectetur. Ab quas distinctio est nesciunt.</p>
+                </div>
+                <div class="col-4">
+                    <aside>
+                        <div class="card mb-3">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" style="border-radius: 50%" class="card-img w-100 h-100" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h3 class="card-title center">Lorena</h5>
+                                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3>Tambien te puede interesar:</h3>
+                        <div class="d-grid gap-2">
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                            <a href="#" class="btn btn-outline-secondary btn-block">Lorem ipsum dolor sit amet</a>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
+</html>
